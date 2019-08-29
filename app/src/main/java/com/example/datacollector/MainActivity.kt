f5272ad5deb9eb6d5a266cc7c8db91d55e6a2165
grider644 <grider644@gmail.com>
2019-08-29 15:44:27 +0530

    Added second activity for recording

--- app/src/main/java/com/example/datacollector/MainActivity.kt
+++ app/src/main/java/com/example/datacollector/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.datacollector
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when user taps on 'Standing' button */
+    fun openStanding(view: View) {
+        //Do something in response
+        val buttonName = findViewById<Button>(R.id.button1)
+        val message = buttonName.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+    /** Called when user taps on 'Walking' button */
+    fun openWalking(view: View) {
+        //Do something in response
+        val buttonName = findViewById<Button>(R.id.button2)
+        val message = buttonName.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+    /** Called when user taps on 'Sitting' button */
+    fun openSitting(view: View) {
+        //Do something in response
+        val buttonName = findViewById<Button>(R.id.button3)
+        val message = buttonName.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+    /** Called when user taps on 'Falling' button */
+    fun openFalling(view: View) {
+        //Do something in response
+        val buttonName = findViewById<Button>(R.id.button4)
+        val message = buttonName.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
